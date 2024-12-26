355b9186a3a843ae33633c5b8045a28212fc723e
reaper <ahoy@barelyhuman.dev>
2024-12-26 13:30:35 +0530

    initial test

---
+++ go.mod
@@ -0,0 +1,5 @@
+module go.reaper.is
+
+go 1.22.2
+
+require github.com/pelletier/go-toml/v2 v2.2.3
---
+++ main.go
@@ -0,0 +1,98 @@
+package main
+
+import (
+	"html/template"
+	"log"
+	"net/url"
+	"os"
+	"path/filepath"
+	"strings"
+
+	"github.com/pelletier/go-toml/v2"
+)
+
+type ConfigDoc struct {
+	Host string
+}
+
+type ConfigServer struct {
+	Url string
+}
+
+type Package struct {
+	Repo        string
+	Description string
+}
+
+type Config struct {
+	Godoc   ConfigDoc
+	Server  ConfigServer
+	Package map[string]Package
+}
+
+type VanityData struct {
+	GoImportContent  string
+	GoRefreshContent string
+	RepoLink         string
+}
+
+func main() {
+	fileData, err := os.ReadFile("./vanity.toml")
+	if err != nil {
+		log.Fatalf("Unable to read vanity.toml with error %v", err)
+	}
+
+	var cfg Config
+	err = toml.Unmarshal(fileData, &cfg)
+	if err != nil {
+		log.Fatalf("Failed to read config with error %v", err)
+	}
+
+	templ := template.Must(
+		template.ParseFiles("./template.html"))
+
+	os.MkdirAll("./dist", os.ModePerm)
+
+	for pkgName, packageDetails := range cfg.Package {
+		fileName := filepath.Join("./dist", pkgName+".html")
+		fd, err := os.OpenFile(fileName, os.O_WRONLY|os.O_CREATE, os.ModePerm)
+		if err != nil {
+			log.Fatalf("Failed to write file %v with error %v", fileName, err)
+		}
+		defer fd.Close()
+		templ.Execute(
+			fd, VanityData{
+				GoImportContent:  constructImportContent(cfg, pkgName, packageDetails.Repo),
+				GoRefreshContent: constructRefreshContent(cfg, pkgName),
+				RepoLink:         packageDetails.Repo,
+			},
+		)
+	}
+}
+
+func constructRefreshContent(cfg Config, pkgName string) string {
+	var sb strings.Builder
+	sb.WriteString("0;URL='")
+	path, err := url.JoinPath(cfg.Godoc.Host, cfg.Server.Url, pkgName)
+	if err != nil {
+		log.Printf("Failed to create refreshContent url for %v", pkgName)
+		return ""
+	}
+	usableUrl, err := url.Parse(path)
+	if err != nil {
+		log.Printf("Failed to create refreshContent url for %v", pkgName)
+		return ""
+	}
+	usableUrl.Scheme = "https"
+	sb.WriteString(usableUrl.String())
+	return sb.String()
+}
+
+func constructImportContent(cfg Config, pkgName string, repoUrl string) string {
+	var sb strings.Builder
+	urlPath, _ := url.JoinPath(cfg.Server.Url, pkgName)
+	sb.WriteString(urlPath)
+	sb.WriteString(" git ")
+	sb.WriteString(repoUrl)
+	return sb.String()
+}
